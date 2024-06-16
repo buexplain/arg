a909c44418c26ef41242e6afaf23d87c60b3b339
buexplain <buexplain@163.com>
2024-06-16 18:28:59 +0800

    优化validate方法

--- src/BaseArgForHyperf.php
+++ src/BaseArgForHyperf.php
@@ -56,8 +56,14 @@ class BaseArgForHyperf extends AbstractArg
         $data = [];
         $rules = [];
         $messages = [];
+        /**
+         * @var ArgProperty[] $otherArg
+         */
+        $otherArg = [];
         foreach ($this->argInfo->getProperties() as $property) {
-            if (!$property->defaultArgClass) {
+            if ($property->defaultArgClass) {
+                $otherArg[] = $property;
+            } else {
                 $data[$property->property->getName()] = $this->{$property->property->getName()};
                 $rules[$property->property->getName()] = implode('|', $property->getRules());
                 $messages = array_merge($messages, $property->getMessages());
@@ -69,23 +75,24 @@ class BaseArgForHyperf extends AbstractArg
             $messages
         );
         //再校验Arg类型的属性
-        if ($validator->passes()) {
-            $messageBag = $validator->getMessageBag();
-            foreach ($this->argInfo->getProperties() as $property) {
-                if ($property->defaultArgClass) {
-                    if ($property->defaultValue === null) {
-                        //如果默认值可以为null，有数据才校验
-                        if ($this->initByParameter[$property->property->getName()]) {
-                            $messageBag->merge($this->{$property->property->getName()}->validate());
-                        }
-                    } else {
-                        //否则直接校验
+        $messageBag = $validator->getMessageBag();
+        if (count($otherArg) > 0 && $messageBag->count() === 0) {
+            foreach ($otherArg as $property) {
+                if ($property->defaultValue === null) {
+                    //如果默认值可以为null，有数据才校验
+                    if ($this->initByParameter[$property->property->getName()]) {
                         $messageBag->merge($this->{$property->property->getName()}->validate());
                     }
+                } else {
+                    //否则直接校验
+                    $messageBag->merge($this->{$property->property->getName()}->validate());
+                }
+                //如果校验失败，则跳出循环
+                if ($messageBag->count() > 0) {
+                    break;
                 }
             }
-            return $messageBag;
         }
-        return $validator->getMessageBag();
+        return $messageBag;
     }
 }
